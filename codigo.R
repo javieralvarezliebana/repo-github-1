bacb7082b67d1303fa9159b8710c31b2d9b7dad7
Javier Álvarez Liébana <93538446+javieralvarezliebana@users.noreply.github.com>
2023-04-18 11:34:12 +0200

    Add files via upload

--- codigo.R
+++ codigo.R
@@ -3,12 +3,12 @@ library(tidyverse)
 datos <- read_csv(file = "https://cnecovid.isciii.es/covid19/resources/casos_hosp_uci_def_sexo_edad_provres.csv",
                   na = "NC")
 
-datos_madrid <-
+datos_bcn <-
   datos |> 
   drop_na(sexo) |> 
-  filter(provincia_iso == "M" & fecha <= "2020-12-31") |> 
+  filter(provincia_iso == "B" & fecha <= "2020-12-31") |> 
   select(fecha, sexo, grupo_edad, num_casos) |> 
   summarise(num_casos = sum(num_casos), .by = c(fecha, sexo))
 
-write_csv(datos_madrid, file = "./datos_madrid.csv")
+write_csv(datos_bcn, file = "./datos_bcn.csv")
   
